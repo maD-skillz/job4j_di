87a7a2b3233b33cfb593bdd6ddb312bd0cd8efe7
maD-skillz <warneverchang3s@gmail.com>
2023-04-26 12:57:06 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -8,10 +8,12 @@ public class SpringDI {
         context.scan("ru.job4j.di");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
+        Store another = context.getBean(Store.class);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
         ui.input("Input");
+        another.getAll().forEach(System.out::println);
     }
 }
 
